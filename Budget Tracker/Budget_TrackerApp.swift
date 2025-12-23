434a443bc6b3310709705ecd3ae566ae2971396f
Viswa Kodela <viswakodela@gmail.com>
2025-12-23 13:41:49 -0500

    Initial Commit

---
+++ Budget Tracker/Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_TrackerApp.swift
+//  Budget Tracker
+//
+//  Created by Viswa Kodela on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
